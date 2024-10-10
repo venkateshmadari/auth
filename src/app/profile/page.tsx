31d8b2143968782a5c2e35ebacdefff1e2ec9aa1
venkateshmadari <venkateshmadari07@gmail.com>
2024-10-10 18:04:12 +0530

    first commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/header.component";
 export default async function Profile() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
-  // console.log(user);
+  console.log(user);
   return (
     <>
       <Header />
